312687761340ccd45bfc1d5c3d4d10318c53a804
wu_zhjian <446440221@qq.com>
2020-02-03 21:57:59 +0800

    wordCount

---
+++ src/main/scala/com/flink/test/StreamWordCount.scala
@@ -0,0 +1,37 @@
+package com.flink.test
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala._
+
+
+object StreamWordCount {
+  def main(args: Array[String]): Unit = {
+    val params = ParameterTool.fromArgs(args)
+    val host: String = params.get("host")
+    val port: Int = params.getInt("port")
+
+    // 创建一个流处理的执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    // 设置并行度
+    // env.setParallelism(1)
+    // 禁用全局cache
+    // env.disableOperatorChaining()
+
+    // 接受socket数据流
+    val textDataStream = env.socketTextStream(host, port)
+
+    import org.apache.flink.api.scala._
+    // 逐一读取数据，分词之后进行wordCount
+    val wordCountDataStream = textDataStream.flatMap(_.split(" "))
+      .filter(_.nonEmpty).startNewChain()
+      .map((_, 1))
+      .keyBy(0)
+      .sum(1)
+
+    // 打印输出
+    wordCountDataStream.print().setParallelism(1)
+
+    // 执行任务
+    env.execute("stream word count job")
+  }
+}
--- src/main/scala/com/flink/test/WordCount.scala
+++ src/main/scala/com/flink/test/WordCount.scala
@@ -1,9 +1,27 @@
 package com.flink.test
 
+import org.apache.flink.api.scala._
+
 
 object WordCount {
 
   def main(args: Array[String]): Unit = {
+
+    // 创建一个批处理执行环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // 从文件中读取数据
+    val inputPah = "E:\\idea\\flink_new_year\\src\\main\\resources\\hello.txt"
+    val inputDateSet = env.readTextFile(inputPah)
+
+    val wordCountDataSet = inputDateSet
+      .flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(_._1)
+      .sum(1)
+
+    // 打印输出
+    wordCountDataSet.print()
 
   }
 }
